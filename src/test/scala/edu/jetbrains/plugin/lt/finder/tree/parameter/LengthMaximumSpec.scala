0337cda98a661d4c220dc9b3dccf67073bfd8a03
baydindima <baidin.d1993@gmail.com>
2016-10-08 00:32:52 +0300

    Add scalatest

---
+++ src/test/scala/edu/jetbrains/plugin/lt/finder/tree/parameter/LengthMaximumSpec.scala
@@ -0,0 +1,18 @@
+package edu.jetbrains.plugin.lt.finder.tree.parameter
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FlatSpec, Matchers}
+
+
+/**
+  * Created by Dmitriy Baidin.
+  */
+@RunWith(classOf[JUnitRunner])
+class LengthMaximumSpec extends FlatSpec with Matchers {
+
+  it should "" in {
+    1 shouldEqual 1
+  }
+
+}
